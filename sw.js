2bdca6c132d5cbbb921d1c4b1bb106f54b911e9c
juniorceara1983-stack <juniorceara1983@gmail.com>
2026-04-02 01:24:14 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,8 @@
-// Service Worker para o Gullas PWA
+// Service Worker Gullas - Permite instalação como App
+const CACHE_NAME = 'gullas-v1';
+self.addEventListener('install', (e) => {
+  console.log('Gullas PWA Instalado');
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(fetch(e.request));
+});
